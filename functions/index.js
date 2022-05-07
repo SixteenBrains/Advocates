28d2a45d0158fe06727c62ac5d0b6a56eef8d1f0
imrishuroy <rishukumar.prince@gmail.com>
2022-05-07 12:25:13 +0530

    Started dynamic links integration

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.invite = functions.https
+.onRequest(async (req, res) => {
+    // this funtion url - https://us-central1-<project-id>.cloudfunctions.net/date
+    //https://us-central1-viewyourstories-4bf4d.cloudfunctions.net/date
+
+     const dataQuery = req.query;
+
+     const shareId = dataQuery.shareId;
+
+     const adUrl = dataQuery.adUrl;
+
+     const adId = dataQuery.adId;
+
+     const promoterId = dataQuery.promoterId;
+
+    const promotedAdRef =  admin.firestore().collection('promotedAds').doc(promoterId).collection('ads').doc(adId);
+
+    await promotedAdRef.update({'clicks': admin.firestore.FieldValue.arrayUnion(shareId)}) 
+
+    res.redirect(adUrl);
+
+    });
